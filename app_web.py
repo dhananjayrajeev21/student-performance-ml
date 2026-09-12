c239f627e22521ffa6c1a779d9881252837e139a
dhananjayrajeev21 <dhananjayrajeev21@gmail.com>
2026-09-12 16:12:24 +0530

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -1144,8 +1144,6 @@ def download():
 # ============================================================
 
 if __name__ == "__main__":
-
-    app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000)
+    import os
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
